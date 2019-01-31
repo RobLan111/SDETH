d66c5e1cb8b93a67dfff26b62647fb3f1df0738c
RobLan111 <47219593+RobLan111@users.noreply.github.com>
2019-01-31 17:30:44 +0100

    WEBSERVERSD
    Example of how to use SD and Ethernet card on Arduino shield

---
+++ SDWEBSERVERS/WEBSERVERSD01/WEBSERVERSD01.ino
@@ -0,0 +1,59 @@
+/*--------------------------------------------------------------
+  Program:      eth_websrv_SD
+
+  Description:  Arduino web server that serves up a basic web
+                page. The web page is stored on the SD card.
+  
+  Hardware:     Arduino Uno and official Arduino Ethernet
+                shield. Should work with other Arduinos and
+                compatible Ethernet shields.
+                2Gb micro SD card formatted FAT16
+                
+  Software:     Developed using Arduino 1.0.3 software
+                Should be compatible with Arduino 1.0 +
+                SD card contains web page called index.htm
+   References:   - WebServer example by David A. Mellis and 
+                  modified by Tom Igoe
+                - SD card examples by David A. Mellis and
+                  Tom Igoe
+                - Ethernet library documentation:
+                  http://arduino.cc/en/Reference/Ethernet
+                - SD Card library documentation:
+                  http://arduino.cc/en/Reference/SD
+
+  Date:         10 January 2013
+ 
+  Author:       W.A. Smith, http://startingelectronics.org
+--------------------------------------------------------------*/
+
+#include <SPI.h>
+#include <Ethernet.h>
+#include <SD.h>
+
+// MAC address from Ethernet shield sticker under board
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+IPAddress ip(10, 0, 0, 20); // IP address, may need to change depending on network
+EthernetServer server(80);  // create a server at port 80
+
+File webFile;
+
+void setup()
+{
+    Ethernet.begin(mac, ip);  // initialize Ethernet device
+    server.begin();           // start to listen for clients
+    Serial.begin(9600);       // for debugging
+    
+    // initialize SD card
+    Serial.println("Initializing SD card...");
+    if (!SD.begin(4)) {
+        Serial.println("ERROR - SD card initialization failed!");
+        return;    // init failed
+    }
+    Serial.println("SUCCESS - SD card initialized.");
+    // check for index.htm file
+    if (!SD.exists("index.htm")) {
+        Serial.println("ERROR - Can't find index.htm file!");
+        return;  // can't find index file
+    }
+    Serial.println("SUCCESS - Found index.htm file.");
+}
